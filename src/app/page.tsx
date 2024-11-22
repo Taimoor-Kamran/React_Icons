40c99a2f83645a054e3190367d059dad1c292385
Taimoor-Kamran <muhammadtaimoor2006@gmail.com>
2024-11-22 17:55:50 +0500

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,25 @@
+// import Achievement from "@/component/Achievement/Achievement";
+// import Course from "@/component/coursesSection/Courses";
+// import CustomerTestimonial from "@/component/CustomerTestimomial/CustomerTest";
+import Footer from "@/component/Footer/Footer";
+import Header from "@/component/Header/Header";
+// import Hero from "@/component/hero/Hero";
+import Navbar from "@/component/Navbar/Navbar";
+
+
+
+export default function Home() {
+  return (
+    <div>
+     <Header/>
+      <Navbar/>
+     {/* <Hero/>
+      <Course/>
+     <Achievement/>
+     <CustomerTestimonial/> */}
+     <Footer/>
+    </div>
+    
+
+  )
+}
